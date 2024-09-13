6104915f68d560d12ebe8d6261160b860f1b3bfc
ameltrabelsi <ameltrabelsi90@gmail.com>
2024-09-13 14:40:34 +0100

    create express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT= process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT , ${PORT}` );
+});
